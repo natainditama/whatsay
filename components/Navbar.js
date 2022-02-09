20fe6ffa2e18d12688c171f55539483cf62caea4
natainditama <natainditama@gmail.com>
2022-02-09 10:06:18 +0700

    v1.0.0

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,8 +6,6 @@ import {
   useTheme,
   useMediaQuery,
   Typography,
-  BottomNavigation,
-  BottomNavigationAction,
 } from "@mui/material";
 import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -42,63 +40,35 @@ function Navbar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      {isDesktopMatches ? (
-        <Stack
-          direction="row"
-          sx={{
-            justifyContent: "flex-start",
-            alignItems: "center",
-            width: "100%",
-          }}
+      <Stack
+        direction="row"
+        sx={{
+          justifyContent: "flex-start",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Tabs
+          value={router.pathname}
+          onChange={handleChange}
+          textColor="inherit"
+          indicatorColor="secondary"
         >
-          <Tabs
-            value={router.pathname}
-            onChange={handleChange}
-            textColor="inherit"
-            indicatorColor="secondary"
-          >
-            {NavList.map((item, index) => {
-              return (
-                <Tab
-                  value={item?.path}
-                  label={<Typography>{item?.name}</Typography>}
-                  key={index}
-                  disableTouchRipple
-                  disableRipple
-                  icon={<FontAwesomeIcon icon={item?.icon} size="lg" />}
-                  iconPosition="start"
-                />
-              );
-            })}
-          </Tabs>
-        </Stack>
-      ) : (
-        <Box
-          sx={{
-            position: "fixed",
-            inset: "0",
-            top: "auto",
-            zIndex: "99999999999",
-          }}
-        >
-          <BottomNavigation
-            showLabels
-            value={router.pathname}
-            onChange={handleChange}
-          >
-            {NavList.map((item, index) => {
-              return (
-                <BottomNavigationAction
-                  label={<Typography>{item?.name}</Typography>}
-                  value={item?.path}
-                  key={index}
-                  icon={<FontAwesomeIcon icon={item?.icon} size="lg" />}
-                />
-              );
-            })}
-          </BottomNavigation>
-        </Box>
-      )}
+          {NavList.map((item, index) => {
+            return (
+              <Tab
+                value={item?.path}
+                label={<Typography>{item?.name}</Typography>}
+                key={index}
+                disableTouchRipple
+                disableRipple
+                icon={<FontAwesomeIcon icon={item?.icon} size="lg" />}
+                iconPosition="start"
+              />
+            );
+          })}
+        </Tabs>
+      </Stack>
     </Box>
   );
 }
